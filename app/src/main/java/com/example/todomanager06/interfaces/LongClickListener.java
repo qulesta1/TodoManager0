d255901d552ce9d2d4b8881b87c39a189a134eed
qulesta1 <94120919+qulesta1@users.noreply.github.com>
2022-01-24 21:09:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/todomanager06/interfaces/LongClickListener.java
@@ -0,0 +1,7 @@
+package com.example.todomanager06.interfaces;
+
+import com.example.todomanager06.model.TaskModel;
+
+public interface LongClickListener {
+    void onItemPress(TaskModel taskModel);
+}
